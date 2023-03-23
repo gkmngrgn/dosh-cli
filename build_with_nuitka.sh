40d2399393714588b57474cb1236511333741df7
Gökmen Görgen <gkmngrgn@gmail.com>
2023-03-24 02:33:02 +0300

    fix python path in script.

--- build_with_nuitka.sh
+++ build_with_nuitka.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+PY="$(command -v python3 || command -v python)"
 
 if command -v poetry &> /dev/null
 then
     echo "poetry found in path."
 else
-    python -m pip install --user pipx
-    python -m pipx install poetry
+    $PY -m pip install --user pipx
+    $PY -m pipx install poetry
 fi
 
-OS_NAME=$(python -c 'import platform; print(platform.system().lower())')
-ARCH_TYPE=$(python -c 'import platform; print(platform.machine().lower())')
-PY_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+OS_NAME=$($PY -c 'import platform; print(platform.system().lower())')
+ARCH_TYPE=$($PY -c 'import platform; print(platform.machine().lower())')
+PY_VERSION=$($PY -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
 DOSH_VERSION=$(poetry version --no-ansi --short)
 DIR_NAME="dosh-cli-${OS_NAME}-${ARCH_TYPE}-${DOSH_VERSION}"
 
 echo "---"
-echo "PYTHON VERSION: ${PY_VERSION}"
-echo "DIRECTORY     : ${DIR_NAME}"
+echo "PYTHON PATH    : ${PY}"
+echo "PYTHON VERSION : ${PY_VERSION}"
+echo "DIRECTORY      : ${DIR_NAME}"
 echo "---"
 
 poetry install --no-ansi --no-interaction
